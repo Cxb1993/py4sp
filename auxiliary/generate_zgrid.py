e8acee31115df6aa853f55f98c2d2be36538cd85
Wim Munters <wim.munters@kuleuven.be>
2016-11-14 12:16:57 +0100

    Update linear zgrid generation script to take CLI input

--- auxiliary/generate_zgrid.py
+++ auxiliary/generate_zgrid.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
     
     Script for generating a PSP-Wind compatible vertical grid using linspace (so no special grids or so...)
@@ -7,14 +8,15 @@
 """
 
 import numpy as np
+import sys
 
 Lz = 1
-Nz = 288 
+Nz = np.int(sys.argv[1])
 Nz2 = 2*Nz+1
 
 z = np.linspace(0,Lz,Nz2)
 
-filename = 'ZMESH_288'
+filename = 'ZMESH_'+str(Nz)
 with open(filename, 'w') as file:
     file.write(str(Nz2)+'\n')
     for k in range(z.size):

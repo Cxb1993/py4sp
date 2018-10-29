c6c13183d2f685adcba7f95ac4b0b87162e1a4e5
Wim Munters <wim.munters@kuleuven.be>
2018-10-29 11:06:20 +0100

    Script for generating zmesh for conventionally neutral atmospheric boundary layers

---
+++ auxiliary/generate_zgrid_cnbl.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+    
+    Script for generating a PSP-Wind compatible vertical grid using linspace (so no special grids or so...)
+
+    Date: 07 Aug 2015
+
+"""
+
+import numpy as np
+import sys
+
+Lz = 2000
+Nz = np.int(sys.argv[1])
+Nz2 = 2*Nz+1
+
+z = np.linspace(0,Lz,Nz2)
+
+filename = 'ZMESH_'+str(Nz)
+with open(filename, 'w') as file:
+    file.write(str(Nz2)+'\n')
+    for k in range(z.size):
+        file.write(str(z[k])+'\n')
+
